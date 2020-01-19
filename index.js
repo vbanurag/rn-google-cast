5697a6648381ab6bf0d87382cb0bc53cd9003c60
João Alves <jneto2727@gmail.com>
2020-01-19 10:34:00 -0300

    chore: add commit validators, bump version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import RnGoogleCast from "./lib/CastContext";
-import CastButton from "./lib/CastButton";
-import EventEmitter from "./lib/EventEmitter";
-import Constants from "./lib/Constants";
+import RnGoogleCast from './lib/CastContext';
+import CastButton from './lib/CastButton';
+import EventEmitter from './lib/EventEmitter';
+import Constants from './lib/Constants';
 
 export { CastButton, EventEmitter, Constants };
 export default RnGoogleCast;
